cc5c3c81454b2706a9890f5b084f8cb2948d9c06
Artem Khazanov <tulenka04@gmail.com>
2025-07-03 10:34:17 +0300

    init commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,118 @@
+import { twoSum, isValid, merge, lengthOfLongestSubstring, levelOrder, isAnagram, searchMatrix, hasCycle, isPalindrome } from "../index.js";
+import { describe, expect, jest, test } from '@jest/globals';
+
+class TreeNode {
+    constructor(val, left, right) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+}
+
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+describe('leetCode Tests', () => {
+    function arrayToTree(arr) {
+        let root = null;
+        if (arr.length) {
+            root = new TreeNode(arr[0]);
+            const queue = [root];
+            let i = 1;
+            while (queue.length && i < arr.length) {
+                const current = queue.shift();
+                if (arr[i] !== null && arr[i] !== undefined) {
+                    current.left = new TreeNode(arr[i]);
+                    queue.push(current.left);
+                }
+                i++;
+                if (i < arr.length && arr[i] !== null && arr[i] !== undefined) {
+                    current.right = new TreeNode(arr[i]);
+                    queue.push(current.right);
+                }
+                i++;
+            }
+        }
+        return root;
+    };
+
+    function arrayToCyclicLinkedList(arr, pos) {
+        let head = null;
+        if (arr.length) {
+            head = new ListNode(arr[0]);
+            let current = head;
+            let cycleEntry = null;
+    
+            for (let i = 1; i < arr.length; i++) {
+                current.next = new ListNode(arr[i]);
+                current = current.next;
+                if (i === pos || pos === 0) cycleEntry = current;
+            }
+    
+            if (pos >= 0) current.next = cycleEntry;
+        };
+
+        return head;
+    }
+
+    test('Two Sums test', () => {
+        expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+        expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+        expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+    });
+
+    test('Valid Parentheses test', () => {
+        expect(isValid("()")).toBeTruthy();
+        expect(isValid("()[]{}")).toBeTruthy();
+        expect(isValid("(]")).toBeFalsy();
+        expect(isValid("([])")).toBeTruthy();
+    });
+
+    test('Merge Intervals test', () => {
+        expect(merge([[1, 3], [2, 6], [8, 10], [15, 18]])).toEqual([[1, 6], [8, 10], [15, 18]]);
+        expect(merge([[1, 4], [4, 5]])).toEqual([[1, 5]]);
+    });
+
+    test('Longest Substring Without Repeating Characters test', () => {
+        expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
+        expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
+        expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
+    });
+
+    test('Binary Tree Level Order Traversal test', () => {
+        expect(levelOrder(arrayToTree([3, 9, 20, null, null, 15, 7]))).toEqual([[3], [9, 20], [15, 7]]);
+        expect(levelOrder(arrayToTree([1]))).toEqual([[1]]);
+        expect(levelOrder(arrayToTree([]))).toEqual([]);
+    });
+
+    test('Valid Anagram test', () => {
+        expect(isAnagram("anagram", "nagaram")).toBeTruthy();
+        expect(isAnagram("rat", "car")).toBeFalsy();
+    });
+
+    test('Search a 2D Matrix test', () => {
+        const matrix = [
+            [1, 3, 5, 7],
+            [10, 11, 16, 20],
+            [23, 30, 34, 60]
+        ]
+        expect(searchMatrix(matrix, 3)).toBeTruthy();
+        expect(searchMatrix(matrix, 13)).toBeFalsy();
+    });
+
+    test('Linked List Cycle test', () => {
+        expect(hasCycle(arrayToCyclicLinkedList([3, 2, 0, -4], 1))).toBeTruthy();
+        expect(hasCycle(arrayToCyclicLinkedList([1, 2], 0))).toBeTruthy();
+        expect(hasCycle(arrayToCyclicLinkedList([1], -1))).toBeFalsy();
+    });
+
+    test('Valid Palindrome test', () => {
+        expect(isPalindrome("A man, a plan, a canal: Panama")).toBeTruthy();
+        expect(isPalindrome("race a car")).toBeFalsy();
+        expect(isPalindrome(" ")).toBeTruthy();
+    });
+});
---
+++ index.js
@@ -0,0 +1,73 @@
+// Two Sums
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+export function twoSum(nums, target) {
+    // TODO
+};
+
+
+// Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// 1 Open brackets must be closed by the same type of brackets.
+// 2 Open brackets must be closed in the correct order.
+// 3 Every close bracket has a corresponding open bracket of the same type.
+export function isValid(s) {
+    // TODO
+};
+
+
+// Merge Intervals
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+export function merge(intervals) {
+    // TODO
+};
+
+
+// Longest Substring Without Repeating Characters
+// Given a string s, find the length of the longest substring without duplicate characters.
+export function lengthOfLongestSubstring(s) {
+    // TODO
+};
+
+
+// Binary Tree Level Order Traversal
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+export function levelOrder(root) {
+    // TODO
+};
+
+
+// Valid Anagram 
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+export function isAnagram(s, t) {
+    // TODO
+};
+
+
+// Search in a 2D Matrix 
+// You are given an m x n integer matrix matrix with the following two properties:
+//   Each row is sorted in non-decreasing order.
+//   The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+// You must write a solution in O(log(m * n)) time complexity.
+export function searchMatrix(matrix, target) {
+    // TODO
+};
+
+// Linked List Cycle
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+export function hasCycle(head) {
+    // TODO
+};
+
+// Valid Palindrome 
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+export function isPalindrome(s) {
+    // TODO
+}
